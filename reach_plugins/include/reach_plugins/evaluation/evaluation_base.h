c2c787ecdd5e9f100740097cb41e6c9fe1c96de9
mripperger <michael.ripperger@swri.org>
2018-05-14 19:30:28 -0500

    Created base class for pose evaluation plugins

---
+++ reach_plugins/include/reach_plugins/evaluation/evaluation_base.h
@@ -0,0 +1,49 @@
+#ifndef REACH_PLUGINS_EVALUATION_EVALUATION_BASE
+#define REACH_PLUGINS_EVALUATION_EVALUATION_BASE
+
+#include <boost/shared_ptr.hpp>
+#include <vector>
+#include <xmlrpcpp/XmlRpcValue.h>
+
+namespace reach_plugins
+{
+namespace evaluation
+{
+
+/**
+ * @brief The EvaluationBase class
+ */
+class EvaluationBase
+{
+public:
+
+  EvaluationBase()
+  {
+
+  }
+
+  virtual ~EvaluationBase()
+  {
+
+  }
+
+  /**
+   * @brief initialize
+   * @param config
+   */
+  virtual bool initialize(XmlRpc::XmlRpcValue& config) = 0;
+
+  /**
+   * @brief calculateScore
+   * @param pose
+   * @return
+   */
+  virtual double calculateScore(const std::vector<double>& pose) = 0;
+
+};
+typedef boost::shared_ptr<EvaluationBase> EvaluationBasePtr;
+
+} // namespace evaluation
+} // namespace reach_plugins
+
+#endif // REACH_PLUGINS_EVALUATION_EVALUATION_BASE
