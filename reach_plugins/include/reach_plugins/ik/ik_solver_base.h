483a2e5312ce3fcc2294266f49d95427fb04cbe6
mripperger <michael.ripperger@swri.org>
2018-05-14 19:31:21 -0500

    Created base class for IK solver plugin

---
+++ reach_plugins/include/reach_plugins/ik/ik_solver_base.h
@@ -0,0 +1,53 @@
+#ifndef REACH_PLUGINS_IK_IK_SOLVER_BASE_H
+#define REACH_PLUGINS_IK_IK_SOLVER_BASE_H
+
+#include <boost/optional.hpp>
+#include <vector>
+#include <xmlrpcpp/XmlRpcValue.h>
+#include <Eigen/Dense>
+
+namespace reach_plugins
+{
+namespace ik
+{
+
+/**
+ * @brief Base class solving IK at a given reach study location
+ */
+class IKSolverBase
+{
+public:
+
+  IKSolverBase()
+  {
+
+  }
+
+  virtual ~IKSolverBase()
+  {
+
+  }
+
+  /**
+   * @brief initialize
+   * @param config
+   * @return
+   */
+  virtual bool initialize(XmlRpc::XmlRpcValue& config) = 0;
+
+  /**
+   * @brief solveIKFromSeed
+   * @param seed
+   * @param solution
+   * @return a boost optional type indicating the success of the IK solution and containing the score of the solution
+   */
+  virtual boost::optional<double> solveIKFromSeed(const Eigen::Affine3d& target,
+                                                  const std::vector<double>& seed,
+                                                  std::vector<double>& solution) = 0;
+};
+typedef boost::shared_ptr<IKSolverBase> IKSolverBasePtr;
+
+} // namespace ik
+} // namespace reach_plugins
+
+#endif // REACH_PLUGINS_IK_IK_SOLVER_BASE_H
