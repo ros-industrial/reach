afc7c6e3b860d89f3b5da7c383590f023e3790e2
mripperger <michael.ripperger@swri.org>
2018-05-20 01:50:39 -0500

    documentation fixup for ik solver base

--- reach_plugins/include/reach_plugins/ik/ik_solver_base.h
+++ reach_plugins/include/reach_plugins/ik/ik_solver_base.h
@@ -36,7 +36,8 @@ public:
   virtual bool initialize(XmlRpc::XmlRpcValue& config) = 0;
 
   /**
-   * @brief solveIKFromSeed
+   * @brief solveIKFromSeed attempts to find a valid IK solution for the given target pose starting from the input seed state.
+   * If a solution is found, the resulting IK solution is saved, and the pose is scored according to the specified cost function plugin
    * @param seed
    * @param solution
    * @return a boost optional type indicating the success of the IK solution and containing the score of the solution
@@ -44,6 +45,7 @@ public:
   virtual boost::optional<double> solveIKFromSeed(const Eigen::Affine3d& target,
                                                   const std::vector<double>& seed,
                                                   std::vector<double>& solution) = 0;
+
 };
 typedef boost::shared_ptr<IKSolverBase> IKSolverBasePtr;
 
