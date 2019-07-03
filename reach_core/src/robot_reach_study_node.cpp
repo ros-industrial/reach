bc8d0253bf12a0c93bd409a536e0e7b8a17fdf32
mripperger <michael.ripperger@swri.org>
2019-07-02 23:34:41 -0500

    Updated reach study node to load new parameters

--- reach_core/src/robot_reach_study_node.cpp
+++ reach_core/src/robot_reach_study_node.cpp
@@ -1,119 +1,38 @@
-#include <ros/ros.h>
 #include "reach/core/reach_study.h"
 #include "reach/core/study_parameters.h"
-#include <xmlrpcpp/XmlRpcException.h>
+
+template<typename T>
+bool get(const ros::NodeHandle& nh,
+         const std::string& key,
+         T& val)
+{
+  if(!nh.getParam(key, val))
+  {
+    ROS_ERROR_STREAM("Failed to get '" << key << "' parameter");
+    return false;
+  }
+  return true;
+}
 
 bool getStudyParameters(ros::NodeHandle& nh,
                         reach::core::StudyParameters& sp)
 {
-  if(!nh.getParam("config_name", sp.config_name))
+  if(!get(nh, "config_name", sp.config_name) ||
+     !get(nh, "fixed_frame", sp.fixed_frame) ||
+     !get(nh, "results_directory", sp.results_directory) ||
+     !get(nh, "object_frame", sp.object_frame) ||
+     !get(nh, "pcd_filename", sp.pcd_filename) ||
+     !get(nh, "optimization/radius", sp.optimization.radius) ||
+     !get(nh, "optimization/max_steps", sp.optimization.max_steps) ||
+     !get(nh, "optimization/step_improvement_threshold", sp.optimization.step_improvement_threshold) ||
+     !get(nh, "planning_group", sp.kin_group_name) ||
+     !get(nh, "get_avg_neighbor_count", sp.get_neighbors) ||
+     !get(nh, "compare_dbs", sp.compare_dbs) ||
+     !get(nh, "visualize_results", sp.visualize_results) ||
+     !get(nh, "ik_solver_config", sp.ik_solver_config) ||
+     !get(nh, "display_config", sp.display_config))
   {
-    ROS_ERROR("'config_name' parameter set incorrectly");
-    return false;
-  }
-
-  if(!nh.getParam("fixed_frame", sp.fixed_frame))
-  {
-    ROS_ERROR("'fixed_frame' parameter must be set");
-    return false;
-  }
-
-  if(!nh.getParam("results_directory", sp.results_directory))
-  {
-    ROS_WARN("'results_directory' parameter not set; using default file location");
-    sp.results_directory = "";
-  }
-
-  if(!nh.getParam("object_frame", sp.object_frame))
-  {
-    ROS_ERROR("'object_frame' parameter must be set");
-    return false;
-  }
-
-  if(!nh.getParam("reach_object/mesh_filename", sp.mesh_filename))
-  {
-    ROS_ERROR("Must set 'reach_object/mesh_filename' parameter");
-    return false;
-  }
-
-  if(!nh.getParam("reach_object/pcd_filename", sp.pcd_filename))
-  {
-    ROS_ERROR("Must set 'reach_object/pcd_filename' parameter");
-    return false;
-  }
-
-  if(!nh.getParam("reach_object/optimization_radius", sp.optimization_radius))
-  {
-    ROS_ERROR("Must set 'reach_object/optimization_radius' parameter");
-    return false;
-  }
-
-  if(!nh.getParam("planning_group/kinematics", sp.kin_group_name))
-  {
-    ROS_ERROR("Must set 'planning_group/kinematics' parameter");
-    return false;
-  }
-
-  if(!nh.getParam("planning_group/manipulability", sp.manip_group_name))
-  {
-    ROS_ERROR("Must set 'planning_group/manipulability' parameter");
-    return false;
-  }
-
-  if(!nh.getParam("constraints/get_avg_neighbor_count", sp.get_neighbors))
-  {
-    ROS_ERROR("'constraints/get_avg_neighbor_count' parameter not set");
-    return false;
-  }
-
-  if(!nh.getParam("constraints/distance_threshold", sp.distance_threshold))
-  {
-    ROS_ERROR("'constraints/distance_threshold' parameter must be set");
-    return false;
-  }
-
-  if(!nh.getParam("constraints/compare_dbs", sp.compare_dbs))
-  {
-    ROS_WARN("'constraints/compare_dbs' parameter not set. No databases will be compared");
-  }
-
-  if(!nh.getParam("constraints/cost_function", sp.cost_function))
-  {
-    ROS_WARN("'constraints/cost_function' parameter not set. Defaulting to manipulability only cost function");
-    sp.cost_function = 0;
-  }
-
-  if(!nh.getParam("/visualize_results", sp.visualize_results))
-  {
-    ROS_WARN("'/visualize_results' parameter not set; reach study results will not be visualized.");
-    sp.visualize_results = false;
-  }
-
-  XmlRpc::XmlRpcValue seed_states;
-  if(!nh.getParam("constraints/seed_states", seed_states))
-  {
-    ROS_WARN("'seed_states' parameter not set; using all zeros seed state");
-  }
-  else
-  {
-    sp.seed_states.resize(seed_states.size());
-    try
-    {
-      for(int i = 0; i < seed_states.size(); ++i)
-      {
-        XmlRpc::XmlRpcValue& pose = seed_states[i];
-        sp.seed_states[i].resize(pose.size());
-        for(int j = 0; j < pose.size(); ++j)
-        {
-          sp.seed_states[i][j] = pose[j];
-        }
-      }
-    }
-    catch(XmlRpc::XmlRpcException& ex)
-    {
-      ROS_INFO_STREAM(ex.getMessage());
-      return false;
-    }
+     return false;
   }
 
   return true;
@@ -124,6 +43,9 @@ int main(int argc, char **argv)
   ros::init(argc, argv, "robot_reach_study_node");
   ros::NodeHandle pnh("~"), nh;
 
+  ros::AsyncSpinner spinner(1);
+  spinner.start();
+
   // Get the study parameters
   reach::core::StudyParameters sp;
   if(!getStudyParameters(pnh, sp))
@@ -132,7 +54,7 @@ int main(int argc, char **argv)
   }
 
   // Initialize the reach study
-  reach::core::ReachStudy rs (nh, sp);
+  reach::core::ReachStudy rs (nh);
 
   // Run the reach study
   if(!rs.run(sp))
@@ -140,6 +62,8 @@ int main(int argc, char **argv)
     ROS_ERROR("Unable to perform the reach study");
     return -1;
   }
+
+  ros::waitForShutdown();
 
   return 0;
 }
