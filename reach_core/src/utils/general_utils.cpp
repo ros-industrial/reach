464c9572dc3674c29fd847cdae2a1156b457eb6c
mripperger <michael.ripperger@swri.org>
2019-07-02 23:07:52 -0500

    Added method to general utilties for creating from from point and normal

--- reach_core/src/utils/general_utils.cpp
+++ reach_core/src/utils/general_utils.cpp
@@ -19,6 +19,39 @@ void integerProgressPrinter(std::atomic<int>& current_counter,
   previous_pct = current_pct;
 }
 
+Eigen::Affine3d createFrame(const Eigen::Vector3f& pt,
+                            const Eigen::Vector3f& norm)
+{
+  // Initialize coordinate frame and set XYZ location
+  Eigen::Affine3f p = Eigen::Affine3f::Identity();
+  p.matrix()(0, 3) = pt(0);
+  p.matrix()(1, 3) = pt(1);
+  p.matrix()(2, 3) = pt(2);
+
+  // Create plane from point normal
+  Eigen::Hyperplane<float, 3> plane (norm, Eigen::Vector3f(0, 0, 0));
+
+  // If the normal and global x-axis are not closely aligned
+  if (std::abs(norm.dot(Eigen::Vector3f::UnitX())) < 0.90)
+  {
+    // Project the global x-axis onto the plane to generate the x-axis
+    Eigen::Vector3f x_axis = plane.projection(Eigen::Vector3f::UnitX()).normalized();
+    p.matrix().col(0).head<3>() = x_axis;
+    p.matrix().col(1).head<3>() = norm.cross(x_axis);
+    p.matrix().col(2).head<3>() = norm;
+  }
+  else
+  {
+    // Project the global y-axis onto the plane to generate the y-axis
+    Eigen::Vector3f y_axis = plane.projection(Eigen::Vector3f::UnitY()).normalized();
+    p.matrix().col(0).head<3>() = y_axis.cross(norm);
+    p.matrix().col(1).head<3>() = y_axis;
+    p.matrix().col(2).head<3>() = norm;
+  }
+
+  return p.cast<double>();
+}
+
 } // namespace core
 } // namespace reach
 
