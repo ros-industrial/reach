ba126a92c91af8a7ace93e48f79d375bbf5c61bd
Marc Bestmann <marc.bestmann@dlr.de>
2023-05-02 10:15:57 +0200

    fix demo launch joint visualization

--- reach_ros/launch/setup.launch.py
+++ reach_ros/launch/setup.launch.py
@@ -44,7 +44,9 @@ def generate_launch_description():
         package='joint_state_publisher_gui',
         executable='joint_state_publisher_gui',
         name='joint_state_publisher_gui',
-        output='screen')
+        output='screen',
+        parameters=[{
+            'source_list': ParameterValue(['reach_joints'])}])
 
     rviz_cmd = Node(
         condition=IfCondition(use_rviz),
